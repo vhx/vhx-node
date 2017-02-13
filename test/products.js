f0770ba3fcc7face1b587d3a9e044e6eae6424ef
Scott Robertson <scott@scottusrobus.com>
2017-02-13 12:04:38 -0800

    can delete for now

--- test/products.js
+++ test/products.js
@@ -38,31 +38,4 @@ describe('Products', function() {
       });
   });
 
-  // describe('Create a product', function() {
-  //   it('it should create a product', function(done) {
-  //     vhx.products.create({ title: 'My Product' }, function(err, product) {
-  //       expect(product).to.be.a('object');
-  //       expect(product).to.be.property('_links');
-  //       expect(product).to.be.property('_embedded');
-  //       expect(product.title).to.equal('My Product');
-  //       expect(product._embedded).to.be.property('files');
-  //       done();
-  //     });
-  //   });
-  // });
-
-  // describe('Update a product', function() {
-  //   it('it should update a product', function(done) {
-  //     var desc = 'New Description ' + Math.random();
-  //     vhx.products.update(54379, { description: desc }, function(err, updatedProduct) {
-  //       expect(updatedProduct).to.be.a('object');
-  //       expect(updatedProduct).to.be.property('_links');
-  //       expect(updatedProduct).to.be.property('_embedded');
-  //       expect(updatedProduct.description).to.equal(desc);
-  //       expect(updatedProduct._embedded).to.be.property('files');
-  //       done();
-  //     });
-  //   });
-  // });
-
 });
